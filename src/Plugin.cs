0a4b00c4a192f10960be4a0a5814798eed5f0d4c
InakoDev <inako.studio+gh@gmail.com>
2026-09-19 19:01:27 +0100

    feat(customer-trade): set default sell price to customer asking price

---
+++ src/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace TweaksAndFixes;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {
+    internal new static ManualLogSource Logger;
+
+    private Harmony harmony;
+
+    private void Awake() {
+        harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
