bb4fbd1bc649c1073204da4813706db94a91c474
InakoDev <inako.studio+gh@gmail.com>
2026-09-20 07:52:47 +0100

    fix(customer-trade): use __instance for Harmony patch parameter

--- src/Patches/CustomerTradePatch.cs
+++ src/Patches/CustomerTradePatch.cs
@@ -5,20 +5,20 @@ namespace TweaksAndFixes.Patches;
 [HarmonyPatch(typeof(CustomerTradeCardScreen), nameof(CustomerTradeCardScreen.SetCustomer))]
 public static class CustomerTradePatch {
     [HarmonyPostfix]
-    private static void Postfix(CustomerTradeCardScreen instance, CustomerTradeData customerTradeData) {
+    private static void Postfix(CustomerTradeCardScreen __instance, CustomerTradeData customerTradeData) {
         // Trading uses cards instead of a price, this patch is specifically for prices.
-        if (instance.m_IsTrading) return;
+        if (__instance.m_IsTrading) return;
 
         // We don't want to overwrite the data when the player returns from the "Let Me Think" option.
         if (customerTradeData != null) return;
 
         // Setting the default price to the customers asking price.
-        var price = instance.m_SellCardAskPrice;
+        var price = __instance.m_SellCardAskPrice;
 
         var input = CSingleton<CGameManager>.Instance.m_CurrencyType != EMoneyCurrencyType.Yen
             ? GameInstance.GetPriceString(price, useCurrencySymbol: false)
             : GameInstance.GetPriceString(price, useCurrencySymbol: false, decimalString: "F0");
 
-        instance.OnInputTextUpdated(input);
+        __instance.OnInputTextUpdated(input);
     }
 }
